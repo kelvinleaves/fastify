4445041c798095011e86dff1530051c0dab91f9d
KelvinXu <kelvinleaves@gmail.com>
2022-03-02 18:52:59 +0800

    create login path

--- routes/root.js
+++ routes/root.js
@@ -4,4 +4,8 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+  fastify.get('/login', async function(request, reply) {
+    return { root: true }
+  })
 }
